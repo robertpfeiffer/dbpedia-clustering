1395c3b8821e3df99d8b79ea6b9d3dd896716f92
Robert Pfeiffer <pfeiffer.robert@gmail.com>
2009-06-16 13:58:17 +0200

    sorry Tobi

---
+++ src/de/myhpi/dbpedia_clustering/Byteconverter.java
@@ -0,0 +1,28 @@
+package de.myhpi.dbpedia_clustering;
+
+class Byteconverter{
+	static int bitAt (byte[] bytes, int index)
+	{
+		return (bytes[index/8] >>> (index%8)) & 1;
+	}
+	static int fromSigned (byte val)
+	{
+		if(val < 0)
+			return 256 + val;
+		return val;
+	}
+	static byte toSigned (int val)
+	{
+		if(val > 127)
+			return (byte)(val - 256);
+		return (byte)val;
+	}
+	static byte bitToByte (int val)
+	{
+		return Byteconverter.toSigned(255*val);
+	}
+	static byte ratioToByte (int num, int den)
+	{
+		return Byteconverter.toSigned((255*num)/den);
+	}
+}
